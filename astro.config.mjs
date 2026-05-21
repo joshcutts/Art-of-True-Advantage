57401604b1a18310da95ff6d673949fe19e46dbb
Joshua Cutts <josh.cutts1@gmail.com>
2026-05-21 13:10:34 -0700

    add seo terms and og image for social media share

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://artoftrueadvantage.com/',
   vite: {
     server: {
       allowedHosts: true,
