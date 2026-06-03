1f304afd6c1410055bfeddf069310d013968629f
Joshua Cutts <josh.cutts1@gmail.com>
2026-06-03 16:39:17 -0700

    adjust includeDotfiles config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://artoftrueadvantage.com/',
+  includeDotfiles: true,
+  site: "https://artoftrueadvantage.com/",
   vite: {
     server: {
       allowedHosts: true,
