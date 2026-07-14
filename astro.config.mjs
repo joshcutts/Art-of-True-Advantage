09385b1259a305bb06fe63bddaca7b7591b045c9
Joshua Cutts <josh.cutts1@gmail.com>
2026-07-14 10:54:58 -0700

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   includeDotfiles: true,
   site: "https://artoftrueadvantage.com/",
+
   vite: {
     server: {
       allowedHosts: true,
     },
   },
+
+  integrations: [sitemap()],
 });
